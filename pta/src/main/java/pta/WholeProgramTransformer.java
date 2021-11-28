90bff83bf3ab44522bc3fdd3d44d9cde4d904fc8
cyy9447 <cyy9447@gmail.com>
2021-11-28 19:31:17 +0800

    CHA，但和之前的不兼容，还需要修改

--- pta/src/main/java/pta/WholeProgramTransformer.java
+++ pta/src/main/java/pta/WholeProgramTransformer.java
@@ -5,17 +5,20 @@ import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import soot.javaToJimple.Util;
 import soot.Local;
 import soot.MethodOrMethodContext;
 import soot.Scene;
 import soot.SceneTransformer;
 import soot.SootMethod;
+import soot.Type;
 import soot.Unit;
 import soot.Value;
 import soot.jimple.DefinitionStmt;
 import soot.jimple.IntConstant;
 import soot.jimple.InvokeExpr;
 import soot.jimple.InvokeStmt;
+import soot.jimple.NewArrayExpr;
 import soot.jimple.NewExpr;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.ReachableMethods;
@@ -27,46 +30,59 @@ public class WholeProgramTransformer extends SceneTransformer {
     @Override
     protected void internalTransform(String arg0, Map<String, String> arg1) {
 
-        TreeMap<Integer, Local> queries = new TreeMap<Integer, Local>();
         Anderson anderson = new Anderson();
-
         ReachableMethods reachableMethods = Scene.v().getReachableMethods();
         QueueReader<MethodOrMethodContext> qr = reachableMethods.listener();
 
-        TreeSet<Integer> allocs = new TreeSet<Integer>(); // 鸵鸟
+        Map<Integer, Type> allocs = new TreeMap<>(); // 保存alloc处的类型和id
+        Map<Integer, Type> queries = new TreeMap<>(); // 保存询问的类型
 
         while (qr.hasNext()) {
             SootMethod sm = qr.next().method();
-            int allocId = 0;
+
+            int allocId = 0; // 用于传递allocID
+            boolean underalloc = false; // 记录是否上面有alloc
 
             if (sm.hasActiveBody()) {
                 for (Unit u : sm.getActiveBody().getUnits()) {
+
                     if (u instanceof InvokeStmt) {
                         InvokeExpr ie = ((InvokeStmt) u).getInvokeExpr();
                         if (ie.getMethod().toString().equals("<benchmark.internal.BenchmarkN: void alloc(int)>")) {
                             allocId = ((IntConstant) ie.getArgs().get(0)).value;
-                            allocs.add(allocId);
+                            underalloc = true;
+                            continue;
                         }
                         if (ie.getMethod().toString().equals("<benchmark.internal.Benchmark: void alloc(int)>")) {
                             allocId = ((IntConstant) ie.getArgs().get(0)).value;
-                            allocs.add(allocId);
+                            underalloc = true;
+                            continue;
                         }
                         if (ie.getMethod().toString()
                                 .equals("<benchmark.internal.BenchmarkN: void test(int,java.lang.Object)>")) {
                             Value v = ie.getArgs().get(1);
                             int id = ((IntConstant) ie.getArgs().get(0)).value;
-                            queries.put(id, (Local) v);
+                            queries.put(id, v.getType());
                         }
                         if (ie.getMethod().toString()
                                 .equals("<benchmark.internal.Benchmark: void test(int,java.lang.Object)>")) {
                             Value v = ie.getArgs().get(1);
                             int id = ((IntConstant) ie.getArgs().get(0)).value;
-                            queries.put(id, (Local) v);
+                            queries.put(id, v.getType());
                         }
                     }
                     if (u instanceof DefinitionStmt) {
                         if (((DefinitionStmt) u).getRightOp() instanceof NewExpr) {
+                            // 可能接在alloc后面也可能不会
+                            NewExpr newExp = (NewExpr) (((DefinitionStmt) u).getRightOp());
+                            if (underalloc) {
+                                allocs.put(allocId, newExp.getType());
+                                // or getBaseType?
+                            }
                             anderson.addNewConstraint(allocId, (Local) ((DefinitionStmt) u).getLeftOp());
+                        }
+                        if (((DefinitionStmt) u).getRightOp() instanceof NewArrayExpr) {
+                            // todo
                         }
                         if (((DefinitionStmt) u).getLeftOp() instanceof Local
                                 && ((DefinitionStmt) u).getRightOp() instanceof Local) {
@@ -74,6 +90,7 @@ public class WholeProgramTransformer extends SceneTransformer {
                                     (Local) ((DefinitionStmt) u).getLeftOp());
                         }
                     }
+                    underalloc = false; // 不是
                 }
             }
             // }
@@ -83,22 +100,26 @@ public class WholeProgramTransformer extends SceneTransformer {
 
         try {
             anderson.run();
-            for (Entry<Integer, Local> q : queries.entrySet()) {
+            throw new Exception();
+/*            for (Entry<Integer, Local> q : queries.entrySet()) {
                 TreeSet<Integer> result = anderson.getPointsToSet(q.getValue());
                 answer += q.getKey().toString() + ":";
                 for (Integer i : result) {
                     answer += " " + i;
                 }
                 answer += "\n";
-            }
+            }*/
 
         } catch (Exception e) {
             answer = "";
-            for (Entry<Integer, Local> q : queries.entrySet()) {
+            for (Entry<Integer, Type> q : queries.entrySet()) {
                 Integer testcase = q.getKey();
                 answer += testcase.toString() + ":";
-                for (Integer i : allocs) {
-                    answer += " " + i;
+                for (Entry<Integer, Type> p : allocs.entrySet()) {
+                    Type mergedType = (p.getValue()).merge(q.getValue(), Scene.v());
+                    if (mergedType == q.getValue()) {
+                        answer += " " + p.getKey();
+                    }
                 }
                 answer += "\n";
             }
